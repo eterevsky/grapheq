715db1ca1aa9faa554d8fb0c4ac7eec536f37c54
Oleg Eterevsky <olege@google.com>
2019-05-14 19:36:45 +0200

    Add setters for most parameters, make line width variable.
    Add an example with a wavy circle.

---
+++ examples/test2.rs
@@ -0,0 +1,16 @@
+use grapheq;
+use image::Luma;
+
+fn main() {
+    let style: grapheq::Style<Luma<u8>> = grapheq::Style::new();
+    let style = style.set_xmin(-2.).set_xmax(2.).set_ymin(-2.).set_ymax(2.).set_width(0.002).set_image_width(1024).set_image_height(1024);
+    let buffer = grapheq::plot(
+        &|x, y| x*x + y*y - 1. + 0.1 * (10.*x + 10.*y).sin(),
+        &|x, y| (
+            2. * x + 0.1 * (10.*x + 10.*y).cos() * 10.,
+            2. * y + 0.1 * (10.*x + 10.*y).cos() * 10.,
+        ),
+        &style
+    );
+    buffer.save("b.png").unwrap()
+}
